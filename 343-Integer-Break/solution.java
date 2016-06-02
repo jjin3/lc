3967cc4ed0dc52962a05a1d6514ac8b5801f8d83
jjin3 <jjin3@users.noreply.github.com>
2016-06-02 11:30:43 -0700

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.java
+++ 343-Integer-Break/solution.java
@@ -1,3 +1,25 @@
+/*
+Theoratically, it is better to keep breaking integers as close to 3 as possible (central to 3), it's good to have 2s and 4s. 
+
+Table
+
+n   sum   product
+--------------------
+n=2 1+1   1
+n=3 2+1   2
+n=4 2+2   4  (is this in the test case? My program shouldn't work for this case, but it accidentally works)
+n=5 3+2   6
+n=6 3+3   9
+n=7 3+4   12
+n=8 3+3+2 18
+n=9 3+3+3 27
+n=10 3+3+2+2 3*3*4=36
+n=11 3+3+3+2 54
+n=12 3+3+3+3 81
+...
+*/
+
+
 public class Solution {
     public int integerBreak(int n) {
         int product=1;
