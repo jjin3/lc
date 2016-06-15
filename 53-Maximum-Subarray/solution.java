33cb35f402b92d4e71ac096a75a747aa6c377ab3
jjin3 <jjin3@users.noreply.github.com>
2016-06-14 22:34:59 -0700

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -30,8 +30,7 @@ public class Solution {
         // Step 1: Get the maxSumDiff first, which is presumably maxSumDiff(i-1).
         // Step 2: update the minSum 
         // keep a minimum before sums[i], which is sums[i-1].  The minimum value initialized as the first element of sums. 
-
-
+        
         int[] sums = getSumRange(nums);
         int minSum = Math.min(0,sums[0]); // the current minimum Sum, this value is an "imagined" value before sum[0]. 
         
@@ -39,11 +38,8 @@ public class Solution {
         int maxSumDiff_Prev = sums[0]; // the first element should be the max initially. 
         
         for (int i=1; i<sums.length; i++) {
-            
             maxSumDiff = Math.max(sums[i] - minSum, maxSumDiff_Prev); // greedily find the current maxSumDiff
-
             minSum = Math.min(sums[i], minSum); // update minimum sum.
-            
             maxSumDiff_Prev = maxSumDiff; // save the current maxSumDiff, which will be used as maxSumDiff(i-1) in the next iteration.
         }
         
@@ -60,9 +56,7 @@ public class Solution {
         
         for (int i=1; i<nums.length; i++) {
             sums[i] = sums[i-1] + nums[i];
-            
         }
-        
         return sums;
     }
 }
