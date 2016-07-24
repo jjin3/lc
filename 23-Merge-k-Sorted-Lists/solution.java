014528743451bf29ad6e5599b919e29fd2a536d3
jjin3 <jjin3@users.noreply.github.com>
2016-07-24 15:26:44 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -23,6 +23,8 @@ public class Solution {
         
         // Define a result linkedlist
         ListNode res = null;
+        ListNode tail = null;
+
         
         // Covert a list[] array to Arraylist for simpler removal later. 
         ArrayList<ListNode> input = new ArrayList<ListNode>(lists.length);
@@ -34,32 +36,31 @@ public class Solution {
         }
         
         // If the input lists still have non empty element, go process it. Once the lists eventually diminished, the process completes. 
-        while (input.size() != 0) {
+        while (!minHeap.isEmpty()) {
             // An important question is, how to pop() element and add new element to minHeap? 
             // The first thought is round robin to pop() element from each list, and add to the minHeap. however, this is not correct, think of an exmaple (1->2->3), (11->12->13) , (21->22->23), First time add 1, 11 and 21, the second time add 2, 12 and 22. this will be out of order, 1, 2, 11, 12,...3,...
             // The figure xx shows the idea of getting the right order 
             
             // THis is the poll process
-            for (ListNode head : input) {
-                if (head != null) { // The current list is not null. 
+
                     // before add new element to the minHeap, Extract minimum value from minHeap: minHeap.poll()
                     // This guarantees to be the smallest among all candidate nodes because we add the first node of all lists to minHeap as the initial state. 
-                    ListNode node = minHeap.poll(); 
+                ListNode node = minHeap.poll(); 
                     
                     // right after it polls, immediately add to result list.
-                    if (res == null) 
-                        res = node;
-                    else {
-                        res.next = node;
-                        res = res.next;
-                    }
+                if (res == null) {
+                    res = node;
+                    tail = res;
+                }
+                else {
+                    tail.next = node;
+                    tail = tail.next;
+                }
                     
                     // whoever being polled, I want it's next element: This is the right element being insert to minHeap agai.
                     if (node.next != null)
                         minHeap.add(node.next); // Trim the original lists. This null list should be removed from the input lists array. 
-                }
             }
-        }
         
         return res;
     }
