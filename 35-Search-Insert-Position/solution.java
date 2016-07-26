cb679c7fd6f35a1275cb530e1654cd9f34001717
jjin3 <jjin3@users.noreply.github.com>
2016-07-25 19:57:30 -0700

    [35. Search Insert Position][Accepted]committed by LeetCode Extension

--- 35-Search-Insert-Position/solution.java
+++ 35-Search-Insert-Position/solution.java
@@ -1,25 +1,40 @@
+
+/*
+
+Case 1: Target is in the list.
+[1,3,5,6], 5 → 2
+
+Case 2: Target were to be inserted in the middle.
+[1,3,5,6], 2 → 1
+
+Case 3: Target were to be inserted to the front, before the first element.
+[1,3,5,6], 7 → 4
+
+Case 4: Target were to be inserted after the last element.
+[1,3,5,6], 0 → 0
+
+*/
+
 public class Solution {
     public int searchInsert(int[] nums, int target) {
-        int high = nums.length;
+        int high = nums.length-1;
         int low = 0;
-        int mid = (0 + nums.length)/2;
         
-        if (target > nums[nums.length-1]) return nums.length;
-        
-        while (low < high) {
-            if (target > nums[mid]) {
-                low = mid;
-                mid = (low+high)/2;
+        while (low <= high) {
+            
+            int mid = (low+high)/2;
+            
+            if (target == nums[mid]) {
+                return mid;
+            }
+            else if (target > nums[mid]) {
+                low = mid+1;
             }
             else if (target < nums[mid]) {
-                high = mid;
-                mid = (low+high)/2;
-            }
-            else if (target == nums[mid]) {
-                return mid;
+                high = mid-1;
             }
         }
         
-        return mid;
+        return low;
     }
 }
