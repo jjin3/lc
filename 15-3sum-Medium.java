3d0bd2deb9cda455aa7f6f01178b612968694e9b
sproutoncloud <sproutoncloud@users.noreply.github.com>
2016-12-01 14:06:55 -0800

    Update 15-3sum-Medium.java

--- 15-3sum-Medium.java
+++ 15-3sum-Medium.java
@@ -12,14 +12,11 @@ public class Solution {
         // a+b = -c
         // for each -c (i.e., O(N)), find a = -c-b in the rest of the nlogn
         
-        // S = [-1, 0, 1, 2, -1, -4],
-        // sort = [-4, -1, -1, 0, 1, 2]
-        // c = -4, find twoSum(nums, i+1, nums.length-1, -c)
+        /* Example: S = [-1, 0, 1, 2, -1, -4],
+           sort = [-4, -1, -1, 0, 1, 2]
+           c = -4, find twoSum(nums, i+1, nums.length-1, -c)
+        */
         
-        //Use 3 pointers - i, low, high
-        //Use Hashset to get unique list.
-        //Use Collections.addAll method to copy from HashSet to ArrayList.
-       // Complexity : O(n2), Time : 78 ms
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         
         for (int outer=0; outer<nums.length; outer++) { // num=c
@@ -28,6 +25,8 @@ public class Solution {
             int end = nums.length-1;
             int twoSum = (-1) * nums[outer];
             
+            // avoid duplicate from the outer loop 
+            // Example: [-4, -1, -1, -1, -1, -1, -1, -1, 0, 1], if -1 was visited from outer loop outer=1, we shouldn't vist the rest of "-1" again in the array from outer loop for outer = 2, 3, 4....
             if(outer !=0  && nums[outer] == nums[outer-1]) continue; 
             
             // two sum two pointer solution below
@@ -38,11 +37,14 @@ public class Solution {
                     res.add(Arrays.asList(nums[start], nums[outer], nums[end]));
                     start++;
                     end--;
-                    // Example: [-4, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, ]
+                    // Example: [-4, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1]
+                    // if -1 was visited from outer loop outer=1, we shouldn't vist the rest of "-1" in the array from outer loop. 
+                    // avoid duplicates from inner loop -> start. When outer=1, nums[outer]=-1, nums[start]=-1 when start=2, we shouldn't visit the rest of "-1" again from inner loop where start = 3, 4, 5...
                     while((start < end) && (nums[start] == nums[start-1]))
-                        start++;// avoid duplicates
+                        start++;  
+                    // avoid duplicates from inner loop <- end    
                     while((start < end) && (nums[end] == nums[end+1]))  // this end+1 will not go out of bound
-                        end--;// avoid duplicates
+                        end--;
                 }
                 
                 else if (nums[start] + nums[end] < twoSum) {
