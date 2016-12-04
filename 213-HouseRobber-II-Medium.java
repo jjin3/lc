1597999efb4ec4fcd3939f1dd10b85d4d286f9c2
sproutoncloud <sproutoncloud@users.noreply.github.com>
2016-12-04 08:01:56 -0800

    Update 213-HouseRobber-II-Medium.java

--- 213-HouseRobber-II-Medium.java
+++ 213-HouseRobber-II-Medium.java
@@ -6,10 +6,12 @@ public class Solution {
         if (nums.length==1)  return nums[0];
         if (nums.length==2)  return Math.max(nums[0], nums[1]);
         
-                // To draw a circle, we found that if the first house (idx=0) is robbed, the thief would not select the last house (idx=nums.length-1). If the first house (idx=0) is NOT robbed, on the other hand, the thief is free to select (idx=nums.length-1). Let's image two arrays with index (0, length-2) and (1, length-1). These are two cases we would need to consider in this problem. It would not be more complicated than two cases, definitely NOT to think of a circulated array with n+1, n+2... 2n+1, 2n+2 etc.
-        
-        System.out.println(rob(nums, 0, nums.length-2));
-        System.out.println(rob(nums, 1, nums.length-1));
+        /* To draw a circle, we found that if the first house (idx=0) is robbed, the thief would not select the 
+        last house (idx=nums.length-1). If the first house (idx=0) is NOT robbed, on the other hand, the thief is 
+        free to select (idx=nums.length-1). Let's image two arrays with index (0, length-2) and (1, length-1). 
+        These are two cases we would need to consider in this problem. It would not be more complicated than two cases,
+        definitely NOT to think of a circulated array with n+1, n+2... 2n+1, 2n+2 etc.
+        */
         return Math.max(rob(nums, 0, nums.length-2), rob(nums, 1, nums.length-1));
     }
     
