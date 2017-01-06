3964beab25251b3f1ffffc373c923046efe8a001
sproutoncloud <sproutoncloud@users.noreply.github.com>
2017-01-06 14:18:56 -0800

    Rename 153-FindMinRotatedArray.java to 153-FindMinRotatedArray-Explaination.java

---
+++ binaraysearch/153-FindMinRotatedArray-Explaination.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1; 
+        
+        while (start <= end) {
+            int mid = start + (end-start)/2; 
+            
+            // MID is the minimum. 
+            if (mid > 0 && nums[mid] < nums[mid-1])
+                return nums[mid];
+                
+            // go to the right half
+            if (nums[mid] >= nums[start] && nums[mid] > nums[end])
+                start = mid+1; 
+            
+            // go to the left half for all other cases; 
+            
+            /*  Explaining why all other cases fall under this scenario. 
+            start<mid<end
+                &                 
+              &           
+            &           
+            
+            start>mid and end>mid
+             &
+                  &
+                &
+                
+            start=mid and end<mid
+             &
+                  
+                &    
+            
+            start=mid and end>mid
+               &
+            &       
+            
+            */
+            else
+                end = mid-1; 
+            
+        }
+        
+        // if start and end meet, the meet point is the minimum. 
+        return nums[start]; 
+        
+    }
+}
